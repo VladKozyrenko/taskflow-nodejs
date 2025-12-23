dd21013d23cd700099596f6610ce919e8c1d035b
Vlad Kozyrenko <kozyrenko.vladyslav@gmail.com>
2025-12-23 17:20:32 +0200

    feat: start express server for lab 3

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import usersRouter from './routes/users.routes';
+import projectsRouter from './routes/projects.routes';
+import tasksRouter from './routes/tasks.routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/users', usersRouter);
+app.use('/projects', projectsRouter);
+app.use('/tasks', tasksRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

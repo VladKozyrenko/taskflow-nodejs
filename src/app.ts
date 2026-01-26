2d77babf6d2cf3d3c78f116a35d8218369169bae
Vlad Kozyrenko <kozyrenko.vladyslav@gmail.com>
2026-01-26 12:32:49 +0200

    refactor: wire task service via dependency injection in app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 import express from 'express';
+
 import usersRouter from './routes/users.routes';
 import projectsRouter from './routes/projects.routes';
-import tasksRouter from './routes/tasks.routes';
+import { createTasksRouter } from './routes/tasks.routes';
+
+import { TaskRepository } from './repositories/task.repository';
+import { ProjectsRepository } from './repositories/projects.repository';
+import { TaskService } from './services/task.service';
+
+import { errorMiddleware } from './middlewares/error.middleware';
 
 const app = express();
-
 app.use(express.json());
 
+// ---- Composition root ----
+const taskRepo = new TaskRepository();
+const projectsRepo = new ProjectsRepository();
+const taskService = new TaskService(taskRepo, projectsRepo);
+
+// Existing routers
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
-app.use('/tasks', tasksRouter);
+
+// Tasks router with injected service
+app.use('/tasks', createTasksRouter(taskService));
+
+// Global error handler (must be last)
+app.use(errorMiddleware);
 
 export default app;
